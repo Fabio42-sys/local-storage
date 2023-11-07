e3fcbe128a9208ec7115a5296517965aee321b4d
Fábio Luiz <142447343+Fabio42-sys@users.noreply.github.com>
2023-11-07 18:53:01 +0000

    fix: comentando o passo a passo do codigo

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// Executa antes das funções.
+
 const dados = JSON.parse(localStorage.getItem('dados'));
 
     document.getElementById('tela').innerHTML = `Nome: ${dados.nome} <br>`;
     document.getElementById('tela').innerHTML += `Idade: ${dados.idade} <br>`;
     document.getElementById('tela').innerHTML += `Time: ${dados.time} <br>`;
+    
+// Essa é a função responsavel por gravar os dados ( Usando a "const dados = {}" você pode colocar mais de um chave (key) ).
 
 function gravaDados() {
 
@@ -19,13 +23,18 @@ function gravaDados() {
         }
     }
 
+// Aqui usando a chave (key) dados nós apresentamos os valores (nome, idade e time) na tela do usuario.
     document.getElementById('tela').innerHTML = `Nome: ${dados.nome} <br>`;
     document.getElementById('tela').innerHTML += `Idade: ${dados.idade} <br>`;
     document.getElementById('tela').innerHTML += `Time: ${dados.time} <br>`;
 
+// Aqui é apresentado na tela do usuario o valor que foi guardado no local-storage.
+
     localStorage.setItem('dados', JSON.stringify(dados));
 
 }
+
+// Com esta função nós apagamos os dados que foram gravados tanto na tela do usuario quando o local-storage da maquina.
 
 function apagarDados() {
 
