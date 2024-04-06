ef3d70709076230094516d95fba264526623d74f
Fábio Luiz <142447343+Fabio42-sys@users.noreply.github.com>
2024-04-06 22:24:55 +0000

    fix: arrumado os erros de js feat: criando o estilo da pagina

--- script.js
+++ script.js
@@ -1,44 +1,39 @@
-// Executa antes das funções.
-
-const dados = JSON.parse(localStorage.getItem('dados'));
-
-    document.getElementById('tela').innerHTML = `Nome: ${dados.nome} <br>`;
-    document.getElementById('tela').innerHTML += `Idade: ${dados.idade} <br>`;
-    document.getElementById('tela').innerHTML += `Time: ${dados.time} <br>`;
-    
-// Essa é a função responsavel por gravar os dados ( Usando a "const dados = {}" você pode colocar mais de um chave (key) ).
+// Essa é a função responsável por gravar os dados ( Usando a "const dados = {}" você pode colocar mais de um chave (key) ).
 
 function gravaDados() {
-
     const dados = {
         pessoa1: {
             nome: document.getElementById('nome').value,
             idade: document.getElementById('idade').value,
             time: document.getElementById('time').value,
-        },
-        pessoa2: {
-            nome: document.getElementById('nome').value,
-            idade: document.getElementById('idade').value,
-            time: document.getElementById('time').value,
         }
-    }
+    };
 
-// Aqui usando a chave (key) dados nós apresentamos os valores (nome, idade e time) na tela do usuario.
-    document.getElementById('tela').innerHTML = `Nome: ${dados.nome} <br>`;
-    document.getElementById('tela').innerHTML += `Idade: ${dados.idade} <br>`;
-    document.getElementById('tela').innerHTML += `Time: ${dados.time} <br>`;
-
-// Aqui é apresentado na tela do usuario o valor que foi guardado no local-storage.
-
+    // Armazenar os dados no localStorage convertendo para string JSON
     localStorage.setItem('dados', JSON.stringify(dados));
 
+    // Exibir os dados na tela
+    document.getElementById('tela').innerHTML = `Nome: ${dados.pessoa1.nome} <br>`;
+    document.getElementById('tela').innerHTML += `Idade: ${dados.pessoa1.idade} <br>`;
+    document.getElementById('tela').innerHTML += `Time: ${dados.pessoa1.time} <br>`;
 }
 
-// Com esta função nós apagamos os dados que foram gravados tanto na tela do usuario quando o local-storage da maquina.
+// Com esta função nós apagamos os dados que foram gravados tanto na tela do usuário quanto no local-storage da máquina.
 
 function apagarDados() {
-
-    localStorage.removeItem('nome');
+    localStorage.removeItem('dados');
     document.getElementById('tela').innerHTML = '';
-
 }
+
+let gravar = document.getElementById('gravar');
+let apagar = document.getElementById('apagar');
+
+gravar.addEventListener('click', () => {
+    document.getElementById('tela').classList.remove('tela-clear');
+    document.getElementById('tela').classList.add('tela');
+});
+
+apagar.addEventListener('click', () => {
+    document.getElementById('tela').classList.remove('tela');
+    document.getElementById('tela').classList.add('tela-clear');
+});
